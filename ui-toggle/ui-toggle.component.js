54007a53dbef63c79ed49910681f2c522b2524c8
cheese-cake-cookie <59796421+cheese-cake-cookie@users.noreply.github.com>
2020-03-12 15:52:01 +0900

    ui-toggle 생성
    원하는 color값 설정 가능

--- ui-toggle/ui-toggle.component.js
+++ ui-toggle/ui-toggle.component.js
@@ -1,1 +1,46 @@
-
+(function () {
+    Vue.component('ui-toggle', {
+        props: {
+            checked: {
+                type: Boolean,
+                default: false
+            },
+            toggleColor: {
+                type: String,
+                default: "#212121"
+            },
+            round: {
+                type: Boolean,
+                default: true
+            },
+            name: {
+                type: String,
+                default: null
+            }
+        },
+        data: function () {
+            return {
+                backgroundColor: "#e6e7e8",
+                sliderStyle: null
+            }
+        },
+        watch: {
+            checked: function (newValue) {
+                if (newValue) {
+                    this.sliderStyle = {
+                        'background-color': this.toggleColor
+                    }
+                } else {
+                    this.sliderStyle = {
+                        'background-color': this.backgroundColor
+                    }
+                }
+            }
+        },
+        template: '' +
+            '<label class="ui-toggle" :class="{round: round}">' +
+                '<slot></slot>' +
+                '<span class="slider" :style="sliderStyle"></span>' +
+            '</label>'
+    });
+}) ();
